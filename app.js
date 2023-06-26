dae1943ea997a303a5fc4548338aa6441344b0ed
Malte Bär <maltebaer@posteo.de>
2023-06-26 23:32:39 +0200

    removes apexcharts, adds open-props

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+const debug = [
+    new Date('2023-06-21T12:00:21.817Z'),
+    new Date('2023-06-23T12:10:21.817Z'),
+    new Date('2023-06-23T18:10:21.817Z'),
+];
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('timestamps', function () {
         return {
             name: this.$persist(''),
 
+            // data: debug,
             data: this.$persist([]),
 
             values: [],
@@ -14,13 +21,8 @@ document.addEventListener('alpine:init', () => {
 
                 this.values = this.getTimestampDifferences(this.data)
 
-                let chart = new ApexCharts(this.$refs.chart, this.options)
-
-                chart.render()
-
                 this.$watch('data', () => {
                     this.values = this.getTimestampDifferences(this.data)
-                    chart.updateOptions(this.options)
                 })
             },
 
@@ -36,29 +38,57 @@ document.addEventListener('alpine:init', () => {
                     const currentTimestamp = new Date(data[i])
                     const nextTimestamp = new Date(data[i + 1])
 
-                    const diffMilliseconds = nextTimestamp - currentTimestamp
-                    const diffMinutes = Math.round(diffMilliseconds / (1000 * 60))
+                    const diffInMilliseconds = nextTimestamp - currentTimestamp
 
-                    values.push({ x: currentTimestamp.toLocaleDateString(), y: diffMinutes })
+                    values.push({ x: currentTimestamp.toLocaleDateString(), y: diffInMilliseconds })
                 }
 
                 // Add the current time for the last missing timestamp
                 const lastTimestamp = new Date(data[data.length - 1])
                 const currentTime = new Date()
-                const diffMilliseconds = currentTime - lastTimestamp
-                const diffMinutes = Math.round(diffMilliseconds / (1000 * 60))
 
-                values.push({ x: lastTimestamp.toLocaleDateString(), y: diffMinutes })
+                const diffInMilliseconds = currentTime - lastTimestamp
+
+                values.push({ x: lastTimestamp.toLocaleDateString(), y: diffInMilliseconds })
 
                 return values
             },
 
-            get options() {
-                return {
-                    chart: { type: 'bar', toolbar: false },
-                    series: [{ data: this.values, }],
+            getSeconds(milliseconds) {
+                return Math.floor(milliseconds / (1000))
+            },
+            getMinutes(milliseconds) {
+                return Math.floor(milliseconds / (1000 * 60))
+            },
+            getHours(milliseconds) {
+                return Math.floor(milliseconds / (1000 * 60 * 60))
+            },
+            getDays(milliseconds) {
+                return Math.floor(milliseconds / (1000 * 60 * 60 * 24))
+            },
+            getWeeks(milliseconds) {
+                return Math.floor(milliseconds / (1000 * 60 * 60 * 24 * 7))
+            },
+
+            getDisplayValue(milliseconds) {
+                const seconds = this.getSeconds(milliseconds)
+                const minutes = this.getMinutes(milliseconds)
+                const hours = this.getHours(milliseconds)
+                const days = this.getDays(milliseconds)
+                const weeks = this.getWeeks(milliseconds)
+
+                if (weeks > 0) {
+                    return weeks + " w"
+                } else if (days > 0) {
+                    return days + " d"
+                } else if (hours > 0) {
+                    return hours + " h"
+                } else if (minutes > 0) {
+                    return minutes + " min"
+                } else {
+                    return seconds + " s"
                 }
-            }
+            },
         }
     })
 })
